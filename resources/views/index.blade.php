03b6df9ef39049f0f74b708905fdc77bb86a940c
Juliano Ferreira Rodrigues <julianof.rodrigues@hotmail.com>
2020-10-27 12:06:11 -0300

    modal de confirmação de delete

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -21,7 +21,6 @@
     <div class="container">
     <div class="row">
 
-
       @foreach($mangas as $manga)
         <div class="col-4">
               <div class="card text-white bg-dark mb-3" style="width: 18rem;">
@@ -30,23 +29,40 @@
                   <h5 class="card-title">{{$manga->name}}</h5>
                   <p class="card-text">{{$manga->description}}</p>
                   <a href="/edit/{{$manga->id}}" class="btn btn-primary">Editar</a>
-                  <a href="/delete/{{$manga->id}}" method="get" class="btn btn-danger">Excluir</a>
+                  <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#exampleModal">
+                    Excluir
+                  </button>
+                  <!-- <a href="/delete/{{$manga->id}}" method="get" class="btn btn-danger">Excluir</a> -->
               </div>
             </div>
         </div>
 
+        <!-- Modal -->
+          <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">{{$manga->name}}</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                  Deseja realmente excluir {{$manga->name}}?
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-danger" data-dismiss="modal">Não</button>
+                  <a href="/delete/{{$manga->id}}" method="get" class="btn btn-success">Sim</a>
+                </div>
+              </div>
+            </div>
+          </div>
       @endforeach
 
       </div>
     </div>
     </div>
-      
 
-      
-
-    
-
-    
     <!-- JavaScript (Opcional) -->
     <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
